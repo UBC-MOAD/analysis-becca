bc5b09b1b035fd4fa20801d540d455843ac16602
Rebecca Beutel <rbeutel@eoas.ubc.ca>
2021-12-17 10:33:33 -0800

    2018 ad tide sback run look like its working, chose spots for particle location in qualitative run

--- ArtificialTides/BaroclinicBarotropic-BC12.py
+++ ArtificialTides/BaroclinicBarotropic-BC12.py
@@ -22,40 +22,41 @@ e3v = e3v*0.5
 e3v = e3v.rename({'z': 'depthv'})
 
 # set runing dates:
-startday = [dt.datetime(2015,11,22)+dt.timedelta(days=i) for i in range(int(1498))]
+startday = [dt.datetime(2017,12,31)+dt.timedelta(days=i) for i in range(int(399))]
 # print(len(startday))
-folders = [dt.datetime(2015,11,29)+dt.timedelta(days=7*i) for i in range(int(214))]
+folders = [dt.datetime(2017,12,31)+dt.timedelta(days=7*(i+1)) for i in range(int(57))]
 folders = np.repeat(folders,7)
 # print(len(folders))
 
-for i in range(131,len(startday)-1):
+for i in range(399): #only want going for just over a year for now
 # all within a for loop so you dont have to restart the code every day for 4 years
 
     # dates for each run
-    date_list = [startday[i],startday[i+1]]
-#     print(str(date_list))
-    folderday = [folders[i], folders[i+1]]
-#     print(str(folderday))
+    date_list_daily = [startday[i],startday[i+1]]
+    folderday_daily = [folders[i], folders[i+1]]
+    date_list_hourly = [startday[i+1],startday[i+2]]
+    folderday_hourly = [folders[i+1], folders[i+2]]
 
     # In[3]:
     #load U
     path = Path("/ocean/mdunphy/CIOPSW-BC12/")
 
     drop_vars = (
-        "sbu", "tauuo", "time_counter_bounds", "time_instant_bounds", "uos",
+        "sbu", "tauuo", "time_counter_bounds", "time_instant_bounds", "uos", "time_instant",
     )
 
-    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1h_grid_U_2D_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday[i], date_list[i], date_list[i]))) for i in range(len(date_list))]
+    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1h_grid_U_2D_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday_hourly[i], date_list_hourly[i], date_list_hourly[i]))) for i in range(len(date_list_hourly))]
+#     print(files)
 
     mydata = xr.open_mfdataset(files, drop_variables=drop_vars)
     ut_h = mydata['ubar']
 
     # load V
     drop_vars = (
-        "sbv", "tauvo", "time_counter_bounds", "time_instant_bounds", "vos",
+        "sbv", "tauvo", "time_counter_bounds", "time_instant_bounds", "vos", "time_instant",
     )
 
-    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1h_grid_V_2D_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday[i], date_list[i], date_list[i]))) for i in range(len(date_list))]
+    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1h_grid_V_2D_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday_hourly[i], date_list_hourly[i], date_list_hourly[i]))) for i in range(len(date_list_hourly))]
 
     mydata = xr.open_mfdataset(files, drop_variables=drop_vars)
     vt_h = mydata['vbar']
@@ -69,7 +70,7 @@ for i in range(131,len(startday)-1):
         'time_instant_bounds', 
     )
 
-    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1d_grid_U_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday[i], date_list[i], date_list[i]))) for i in range(len(date_list))]
+    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1d_grid_U_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday_daily[i], date_list_daily[i], date_list_daily[i]))) for i in range(len(date_list_daily))]
 
     mydata = xr.open_mfdataset(files, drop_variables=drop_vars)
     u_d = mydata['uo']
@@ -79,10 +80,14 @@ for i in range(131,len(startday)-1):
         'time_instant_bounds', 
     )
 
-    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1d_grid_V_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday[i], date_list[i], date_list[i]))) for i in range(len(date_list))]
+    files = [sorted(path.glob("{:%Y%m%d}00/BC12_1d_grid_V_{:%Y%m%d}_{:%Y%m%d}.nc".format(folderday_daily[i], date_list_daily[i], date_list_daily[i]))) for i in range(len(date_list_daily))]
 
     mydata = xr.open_mfdataset(files, drop_variables=drop_vars)
     v_d = mydata['vo']
+    
+    #lets mask both daily files so all the land values are nan
+    u_d = u_d.where(u_d != 0)
+    v_d = v_d.where(v_d != 0)
 
     #calcuate barotropic component
     ut_d = (u_d*e3u[0,:,:,:]).sum(dim='depthu')/e3u[0,:,:,:].sum(dim='depthu')
@@ -92,7 +97,7 @@ for i in range(131,len(startday)-1):
 
 
     # interpolate + resample uc_d to get it in an hourly format
-    offset = dt.timedelta(hours=-23) # daily avg timestamp as next day time 0, offset by -23 hours to line up with hourly data
+    offset = dt.timedelta(hours=1)
     uc_h_interp = uc_d.resample(time_counter="1H", loffset=offset).interpolate("linear")
     #instead of taking 12hours off each side like when done with SSC, this method takes off 24hours on the end!
 
@@ -102,11 +107,15 @@ for i in range(131,len(startday)-1):
     u_new = u_new.rename('vozocrtx') #name it what you want it named in final netcdf
 
     # And save!
-    # np.save("u_new.npy",u_new)
+    encoding={
+          "vozocrtx": {"zlib": True, "complevel": 4, "_FillValue": 0}
+    }
+    
     path = '/ocean/rbeutel/data/'
-    u_new.to_netcdf(str(path)+'{:%Y%m}/U_new_{:%Y%m%d}.nc'.format(date_list[0],date_list[0]))
-#     print('u_new_{:%d%b%y}_{:%d%b%y}.nc complete'.format(date_list[0],date_list[-1]))
-    # Now for V
+    u_new.to_netcdf(str(path)+'{:%Y%m}/U_new_{:%Y%m%d}.nc'.format(date_list_hourly[0],date_list_hourly[0]), encoding=encoding)
+    
+    
+    # Now for V!
 
     #calcuate bartropic component
     vt_d = (v_d*e3v[0,:,:,:]).sum(dim='depthv')/e3v[0,:,:,:].sum(dim='depthv')
@@ -118,14 +127,18 @@ for i in range(131,len(startday)-1):
     # vc_d.load(scheduler="processes", num_workers=n)
 
     # interpolate + resample uc_d to get it in an hourly format
-    offset = dt.timedelta(hours=-23) # daily avg timestamp as next day time 0, offset by -23 hours to line up with hourly data
+    offset = dt.timedelta(hours=1) 
     vc_h_interp = vc_d.resample(time_counter="1H", loffset=offset).interpolate("linear")
     #instead of taking 12hours off each side like when done with SSC, this method takes off 24hours on the end!
 
     v_new = vt_h  + vc_h_interp
     v_new = v_new.isel(time_counter = np.arange(0,24,1)) #remove extra hour 
     v_new = v_new.rename('vomecrty') #name it what you want it named in final netcdf
+    
+    encoding={
+          "vomecrty": {"zlib": True, "complevel": 4, "_FillValue": 0}
+    }
 
     # np.save("v_new.npy",v_new)
-    v_new.to_netcdf(str(path)+'{:%Y%m}/V_new_{:%Y%m%d}.nc'.format(date_list[0],date_list[0]))
+    v_new.to_netcdf(str(path)+'{:%Y%m}/V_new_{:%Y%m%d}.nc'.format(date_list_hourly[0],date_list_hourly[0]), encoding=encoding)
 #     print('v_new_{:%d%b%y}_{:%d%b%y}.nc complete'.format(date_list[0],date_list[-1]))
