cc131592027508cee4127a5a7e7067a022dda918
Rebecca Beutel <rbeutel@eoas.ubc.ca>
2022-03-08 14:10:10 -0800

    compared babypoo to Uart, Vart, and tmask

--- ArtificialTides/BaroclinicBarotropic-BC12.py
+++ ArtificialTides/BaroclinicBarotropic-BC12.py
@@ -29,15 +29,15 @@ xmesh_v = mydata.rename({'z': 'depthv'})
 
 # set runing dates:
 startday = [dt.datetime(2016,12,25)+dt.timedelta(days=i) for i in range(int(406))]
-startday = startday[154:]
+startday = startday[153:155]
 # print(len(startday))
 folders = [dt.datetime(2016,12,25)+dt.timedelta(days=7*(i+1)) for i in range(int(58))]
 folders = np.repeat(folders,7)
-folders = folders[154:]
+folders = folders[153:155]
 # print(len(folders))
 
 # for i in range(406): #only want going for just over a year for now
-for i in range(252):
+for i in range(2):
 # all within a for loop so you dont have to restart the code every day for 4 years
 
     # dates for each run
